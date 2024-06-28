2a7b310a0462e2889b3e6aee1b9772ce1ffcf19c
Dmitriy <admin@gmail.com>
2024-06-28 23:39:23 +0200

    Make blog

--- src/BLL/store.js
+++ src/BLL/store.js
@@ -16,10 +16,12 @@ const data = {
         },
         PageContent: {
             Post: [
-                {class:"", id:1, img:"https://store-images.s-microsoft.com/image/apps.30323.14537704372270848.6ecb6038-5426-409a-8660-158d1eb64fb0.08703491-f5dc-4b00-bca6-486b7b293c17?q=90&w=480&h=270", text: "Lorem, ipsum dolor sit recusandae similique ipsa xyu."},
-                {class:"post__last-block", id:2, img:"https://ltdfoto.ru/images/2024/06/28/elden.jpg", text: "Lorem, ipsum dolor sit recusandae similique ipsa."}
+                {class:"", id:1, img:"https://store-images.s-microsoft.com/image/apps.30323.14537704372270848.6ecb6038-5426-409a-8660-158d1eb64fb0.08703491-f5dc-4b00-bca6-486b7b293c17?q=90&w=480&h=270", text: "Lorem, ipsum dolor sit recusandae similique ipsa xyu.", boolean: true},
+                {class:"post__last-block", id:2, img:"https://image.api.playstation.com/vulcan/ap/rnd/202402/2214/fe6de6126062e4d38dd6c4cef9ba1966ee2440a43429dc98.jpg", text: "Lorem, ipsum dolor sit recusandae similique ipsa.", boolean: true},
+                
             ]
         }
+
     }
 }
 
